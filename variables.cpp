699e652c820f4ba45e66d0fd012acda6ea058b6c
Fadil <fadilakbar7770@gmail.com>
2022-02-18 08:00:11 +0700

    start "variables" and configuring .gitignore

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float monthlySalary = 5499999.99;
+    float dailySalary = monthlySalary / 30;
+    cout << "Your daily salary is " << dailySalary;
+    system("pause>0");
+}
