6cab9b3f3f423caa27f94476e6953e1bef5c8ff3
Fadil <fadilakbar7770@gmail.com>
2022-02-19 22:19:21 +0700

    finish "datatype overflow"

---
+++ datatype-overflow.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maxInt = INT_MAX;
+    cout << maxInt << endl << "After adding the value of 1 to the maxInt variable, The value of maxInt will become : " << maxInt + 1;
+    system("pause>0");
+}
