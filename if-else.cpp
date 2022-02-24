ccf00b431330c6845fbd9b6e663cc17550b6279f
Fadil <fadilakbar7770@gmail.com>
2022-02-24 22:07:36 +0700

    refactor "if else"

--- if-else.cpp
+++ if-else.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <cmath>
-#include <string>
+
 using namespace std;
 
 int main()
 {
     // prime number checker program
     int number;
+    int result = 1;
     cout << "Please enter a number : ";
     cin >> number;
-    string result = to_string(number) + " is a prime";
-    if(number == 2) cout << result;
-    if(number < 2 || number >= 3 && number % 2 == 0) result = to_string(number) + " is not a prime";
+
+    if(number < 2 || number >= 3 && number % 2 == 0) result = 0;
     for(int i = 3; i <= floor(sqrt(number)); i+=2)
     {
-        if(number % i == 0) result = to_string(number) + " is not a prime";
+        if(number % i == 0) result = 0;
     }
-    cout << result;
+    
+    if(result) cout << number << " is a prime";
+    else cout << number << " is not a prime";
+
     system("pause>0");
 }
